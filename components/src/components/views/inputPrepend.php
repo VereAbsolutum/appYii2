59edf2f028582c813efd98889831d88df17706e9
SP <sergiopile3@gmail.com>
2023-12-01 17:40:58 -0300

    Criado a view de exemplo para ser renderizado com o metodo render implementado do framework na class herdada para os componentes

---
+++ components/src/components/views/inputPrepend.php
@@ -0,0 +1,6 @@
+<div class="input-group mb-3">
+    <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1"><?= $prepend ?></span>
+    </div>
+    <input type="text" class="form-control" placeholder="<?= $content ?>" aria-label="Username" aria-describedby="basic-addon1">
+</div>
