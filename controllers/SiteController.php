ba54166610a66b03c8ba01d23441bff7410e5621
SP <sergiopile3@gmail.com>
2023-12-01 15:19:35 -0300

    Criado a rota component para teste da implementação do component na direto na view.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,6 +64,11 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionComponent()
+    {
+        return $this->render('component');
+    }
+
     /**
      * Login action.
      *
